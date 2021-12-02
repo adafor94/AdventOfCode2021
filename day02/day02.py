44aa5b725767ba40f419f56dc1ff2dde13aa71b8
adam <adamforsberg94@gmail.com>
2021-12-02 08:09:44 +0100

    day02

---
+++ day02/day02.py
@@ -0,0 +1,19 @@
+import fileinput
+
+directions = [line.split(' ') for line in open("input.in", "r").read().strip().split('\n')]
+horizontal = depth1 = depth2 = aim = 0
+
+for dir in directions:
+  value = int(dir[1])
+  if dir[0] == 'down':
+    depth1 += value
+    aim += value
+  elif dir[0] == 'up':
+    depth1 -= value
+    aim -= value
+  elif dir[0] == 'forward':
+    horizontal += value
+    depth2 += aim*value
+
+print("Part1:", depth1*horizontal)
+print("Part2:", depth2*horizontal)
