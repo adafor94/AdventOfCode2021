b52f4735b0d98f5a71186e259ff58bf2e22bfa65
adam <adamforsberg94@gmail.com>
2021-12-02 09:25:18 +0100

    day02 2.1

--- day02/day02.py
+++ day02/day02.py
@@ -3,15 +3,14 @@ import fileinput
 directions = [line.split(' ') for line in open("input.in", "r").read().strip().split('\n')]
 horizontal = depth1 = depth2 = 0
 
-for dir in directions:
-  value = int(dir[1])
-  if dir[0] == 'down':
-    depth1 += value
-  elif dir[0] == 'up':
-    depth1 -= value
-  elif dir[0] == 'forward':
-    horizontal += value
-    depth2 += depth1*value
+for dir, value in directions:
+  if dir == 'down':
+    depth1 += int(value)
+  elif dir == 'up':
+    depth1 -= int(value)
+  elif dir == 'forward':
+    horizontal += int(value)
+    depth2 += depth1*int(value)
 
 print("Part1:", depth1*horizontal)
 print("Part2:", depth2*horizontal)
