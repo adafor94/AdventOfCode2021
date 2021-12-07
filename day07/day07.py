459d5073b86fcb5d5306840dd73624b68ceaed70
adam <adamforsberg94@gmail.com>
2021-12-07 09:27:39 +0100

    day7, same but cleaner

--- day07/day07.py
+++ day07/day07.py
@@ -1,24 +1,13 @@
 import math 
 
 crabs = [int(crab) for crab in open("input.in", "r").read().strip().split(',')]
-crabs.sort()
+crabs.sort()   
+#Part 1
+x = crabs[int((len(crabs)/2)-1)]  # Median value
+print("Part1:", sum(abs(crab-x) for crab in crabs)) #Sum of distance to median value
 
-def cost1(crabs, x):
-  cost = 0
-  for crab in crabs:
-    cost += abs(crab-x)
-  return cost
+#Part2. Answer is the average rounded up or down
+x1, x2 = math.floor(sum(crabs)/len(crabs)), math.ceil(sum(crabs)/len(crabs)) 
 
-def cost2(crabs, x):
-  cost = 0
-  for crab in crabs:
-    n = abs(crab-x)         #Steps
-    cost += n*(n+1)/2       # Sum of all natural numbers 0 to n
-  return cost
-
-x = crabs[int((len(crabs)/2)-1)]  # Middle value of sorted list
-print("Part1:", cost1(crabs, x))
-
-x1 = math.floor(sum(crabs)/len(crabs)) # Average rounded down
-x2 = math.ceil(sum(crabs)/len(crabs))   # Average rounded up
-print("Part2:", int(min(cost2(crabs,x1), cost2(crabs,x2))))
+cost = lambda crabs, x: sum(abs(crab-x)*(abs(crab-x)+1)/2 for crab in crabs) #sum of the costs. Using 1+2+3+..+n = n*(n+1)/2
+print("Part2:", int(min(cost(crabs,x1), cost(crabs,x2))))
